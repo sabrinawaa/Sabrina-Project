a0521a60962250490f6672df070c17178fac0f7e
sabrinawaa <119061814+sabrinawaa@users.noreply.github.com>
2023-02-21 14:32:29 +0100

    fixed interpolation

--- henon_quadratic.py
+++ henon_quadratic.py
@@ -13,36 +13,40 @@ import pandas as pd
 import scipy.optimize as op
 
 
+def hanning_window(x):
+    return x * 2 * np.power((np.sin(np.pi * np.arange(len(x)) / (len(x)))), 2)
+
 def A(a, b, c):
     return (-(a + b * c) * (a - b) + b * np.sqrt(c**2 * (a + b)**2 - 2 * a * b * (2 * c**2 - c - 1))) / (a**2 + b**2 + 2 * a * b * c)
 
 def interpolation(data):
-    index=np.where(data==max(data))[0][0]
+    index = np.argmax(data)
     N=len(data)
     
     if index==0:
-        return 0.0
-    elif (index==len(data)-1):
         return 1.0
+    elif (index== N-1):
+        return 0.0
+    if (data[index - 1] > data[index + 1]):
+        i1 = index - 1
+        i2 = index
+        index = index - 1
     else:
-        if (data[index - 1] >
-            data[index + 1]):
-            i1 = index - 1
-            i2 = index
-            index = index - 1
-        else:
-            i1 = index
-            i2 = index + 1
-        value=   ((index+1)/N)+(1.0/(2.0*np.pi))*np.arcsin(A(data[i1], data[i2], np.cos(2.0*np.pi/N)) * np.sin(2.0*np.pi/N))
-        return abs(value)
+        i1 = index
+        i2 = index + 1
+    value=   (index/N)+(1.0/(2.0*np.pi))*np.arcsin(A(data[i1], data[i2], np.cos(2.0*np.pi/N)) * np.sin(2.0*np.pi/N))
+    return abs(value)
     
 
 def fft_tune(x,px,alf,beta):
     xn=x/np.sqrt(beta)
     pxn=alf*x/np.sqrt(beta) + px*np.sqrt(beta)
+    xn=hanning_window(xn)
+    pxn=hanning_window(pxn)
     coords=xn - 1j * pxn
     freqs=np.fft.fft(coords)
     return interpolation(abs(freqs))
+
 
 
 mad=Madx()
@@ -51,10 +55,7 @@ mad=Madx()
 no_particles=17
 no_turns=8192
 
-strengths=[0.3,0.4,0.5,0.6]
-tunes_all=[ []for i in range (len(strengths))]
-x0_all=[ []for i in range (len(strengths))]
-p0_all=[ []for i in range (len(strengths))]
+
 
 def quad_func(x,a2,a0):
     return a2*np.square(x)+a0
@@ -97,6 +98,11 @@ plt.xlabel("x0")
 plt.ylabel("Q_x")
 #%% also generating data
 
+strengths=[0.3,0.4,0.5,0.6]
+tunes_all=[ []for i in range (len(strengths))]
+x0_all=[ []for i in range (len(strengths))]
+p0_all=[ []for i in range (len(strengths))]
+
 for j in range(len(strengths)):
     with open('job1.madx', 'r') as file:
         data = file.read()
@@ -117,7 +123,10 @@ for j in range(len(strengths)):
     #plt.plot(np.array(twiss.S), np.array(twiss.BETX))
     
     for i in range (1,no_particles+1):
-        name="track.obs0001.p000"+str(i)
+        if i <10:
+            name="track.obs0001.p000"+str(i)
+        else:   
+            name="track.obs0001.p00"+str(i)
         
         track= pd.read_fwf(name, skiprows=6,infer_nrows=no_turns)
         track=track.drop(index=0,columns="*")
