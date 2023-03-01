1c1699693845e729db4d0b07d5c4b0c21a2152b9
Sabrina Wang <sawang@pcbe17364.dyndns.cern.ch>
2023-03-01 09:59:55 +0100

    scan octupoles

--- scan_savetrack.py
+++ scan_savetrack.py
@@ -11,38 +11,37 @@ import os
 
 
 mad=Madx()
-oct_names=[]
-strengths=[0.3,0.4,0.5,0.6,0.7,0.8,0.9]
-no_particles=10
+# #%%
+# oct_names=[]
+# strengths=[0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+# no_particles=10
 
-for j in range(len(strengths)):
-    with open('job1.madx', 'r') as file:
-        data = file.read()
-        data = data.replace("K3=0.1", "K3="+str(strengths[j]))
-    with open('job1.madx', 'w') as file:     
-        file.write(data)
+# for j in range(len(strengths)):
+#     with open('job1.madx', 'r') as file:
+#         data = file.read()
+#         data = data.replace("K3=0.1", "K3="+str(strengths[j]))
+#     with open('job1.madx', 'w') as file:     
+#         file.write(data)
         
-    mad.call("job1.madx")
+#     mad.call("job1.madx")
     
-    for i in range (1,no_particles+1):
-        if i <10:
-            name="track.obs0001.p000"+str(i)
-        else:   
-            name="track.obs0001.p00"+str(i)
+#     for i in range (1,no_particles+1):
+#         if i <10:
+#             name="track.obs0001.p000"+str(i)
+#         else:   
+#             name="track.obs0001.p00"+str(i)
             
-        newname="track.k3=" +str(strengths[j])+"no="+str(i)
-        os.rename(name, newname)
+#         newname="track.k3=" +str(strengths[j])+"no="+str(i)
+#         os.rename(name, newname)
         
-    with open('job1.madx', 'r') as file:
-        data = file.read()
-        data = data.replace("K3="+str(strengths[j]),"K3=0.1")
-    with open('job1.madx', 'w') as file:     
-        file.write(data)
+#     with open('job1.madx', 'r') as file:
+#         data = file.read()
+#         data = data.replace("K3="+str(strengths[j]),"K3=0.1")
+#     with open('job1.madx', 'w') as file:     
+#         file.write(data)
         
-    print('-----------finished running strength=',strengths[j],"---------------")
+#     print('-----------finished running strength=',strengths[j],"---------------")
             
-    
-#%%
 oct_names=["LOE.12002","LOE.22002","LOE.32002","LOEN.52002"]
 strengths=[0.3,0.4,0.5]#,0.6,0.7,0.8,0.9]
 no_particles=1
