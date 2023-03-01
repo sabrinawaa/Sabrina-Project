757c74e5d3f169189866df01b40debd0729c4d0d
Sabrina Wang <sawang@pcbe17364.dyndns.cern.ch>
2023-03-01 14:37:38 +0100

    good ver

--- scan_savetrack.py
+++ scan_savetrack.py
@@ -3,7 +3,7 @@
 """
 Created on Mon Feb 20 11:30:00 2023
 
-@author: sabo4ever
+@author: sabo1ever
 """
 
 from cpymad.madx import Madx
@@ -13,7 +13,7 @@ import os
 mad=Madx()
 # #%%
 # oct_names=[]
-# strengths=[0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+# strengths=[0.3,0.1,0.5,0.6,0.7,0.8,0.9]
 # no_particles=10
 
 # for j in range(len(strengths)):
@@ -44,20 +44,20 @@ mad=Madx()
       # #%%tracking centre   
       
 oct_names=["LOE.12002","LOE.22002","LOE.32002","LOEN.52002"]
-strengths=[0.3,0.4,0.5,0.6,0.7,0.8,0.9]
+strengths=[0.3,0.1,0.5,0.6,0.7,0.8,0.9]
 no_particles=8
 
 for k in oct_names:
     for j in range(len(strengths)):
-        with open('job4.madx', 'r') as file:
+        with open('job1.madx', 'r') as file:
             data = file.read()
             data = data.replace("K3=0.1", "K3="+str(strengths[j]))
             data = data.replace("LOF.30802", k)
             
-        with open('job4.madx', 'w') as file:     
+        with open('job1.madx', 'w') as file:     
             file.write(data)
             
-        mad.call("job4.madx")
+        mad.call("job1.madx")
         
         for i in range (1,no_particles+1):
             if i <10:
@@ -68,11 +68,11 @@ for k in oct_names:
             newname="track.oct="+k+"k3=" +str(strengths[j])+"no="+str(i)
             os.rename(name, newname)
             
-        with open('job4.madx', 'r') as file:
+        with open('job1.madx', 'r') as file:
             data = file.read()
             data = data.replace("K3="+str(strengths[j]),"K3=0.1")
             data = data.replace(k,"LOF.30802")
-        with open('job4.madx', 'w') as file:     
+        with open('job1.madx', 'w') as file:     
             file.write(data)
             
         print('-----------finished running strength=',strengths[j],"---------------")
