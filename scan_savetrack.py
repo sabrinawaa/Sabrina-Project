e098b593960a467b4e3d3cddd023358e067208fb
sabrinawaa <sabrinaziqiwang@yahoo.com>
2023-03-02 17:09:35 +0100

    scan cent about qx=0.75

--- scan_savetrack.py
+++ scan_savetrack.py
@@ -11,19 +11,20 @@ import os
 
 
 mad=Madx()
+job='75track_cent.madx'
 # #%%
 # oct_names=[]
 # strengths=[0.3,0.1,0.5,0.6,0.7,0.8,0.9]
 # no_particles=10
 
 # for j in range(len(strengths)):
-#     with open('job1.madx', 'r') as file:
+#     with open(job, 'r') as file:
 #         data = file.read()
 #         data = data.replace("K3=0.1", "K3="+str(strengths[j]))
-#     with open('job1.madx', 'w') as file:     
+#     with open(job, 'w') as file:     
 #         file.write(data)
         
-#     mad.call("job1.madx")
+#     mad.call(job)
     
 #     for i in range (1,no_particles+1):
 #         if i <10:
@@ -34,31 +35,31 @@ mad=Madx()
 #         newname="track.k3=" +str(strengths[j])+"no="+str(i)
 #         os.rename(name, newname)
         
-#     with open('job1.madx', 'r') as file:
+#     with open(job, 'r') as file:
 #         data = file.read()
 #         data = data.replace("K3="+str(strengths[j]),"K3=0.1")
-#     with open('job1.madx', 'w') as file:     
+#     with open(job, 'w') as file:     
 #         file.write(data)
         
 #     print('-----------finished running strength=',strengths[j],"---------------")
 
       
 oct_names=["LOE.12002","LOE.22002","LOE.32002","LOEN.52002"]
-strengths=[0.3,0.1,0.5,0.6,0.7,0.8,0.9]
+strengths=[0.3,0.6,0.9]#
 no_particles=8
 
 
 for k in oct_names:
     for j in range(len(strengths)):
-        with open('job1.madx', 'r') as file:
+        with open(job, 'r') as file:
             data = file.read()
             data = data.replace("K3=0.1", "K3="+str(strengths[j]))
             data = data.replace("LOF.30802", k)
             
-        with open('job1.madx', 'w') as file:     
+        with open(job, 'w') as file:     
             file.write(data)
             
-        mad.call("job1.madx")
+        mad.call(job)
         
         for i in range (1,no_particles+1):
             if i <10:
@@ -69,11 +70,13 @@ for k in oct_names:
             newname="track.oct="+k+"k3=" +str(strengths[j])+"no="+str(i)
             os.rename(name, newname)
             
-        with open('job1.madx', 'r') as file:
+        with open(job, 'r') as file:
             data = file.read()
             data = data.replace("K3="+str(strengths[j]),"K3=0.1")
             data = data.replace(k,"LOF.30802")
-        with open('job1.madx', 'w') as file:     
+        with open(job, 'w') as file:     
             file.write(data)
             
-        
+        print('-----------finished running strength=',strengths[j],"---------------")
+
+            
