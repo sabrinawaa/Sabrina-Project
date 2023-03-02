5deb494096be678aca3e424e49c52f85a213ff8b
Sabrina Wang <sawang@pcbe17364.dyndns.cern.ch>
2023-03-02 17:21:28 +0100

    work of 302

--- octs_twisstable.py
+++ octs_twisstable.py
@@ -34,7 +34,7 @@ for k in oct_names:
         twiss_sum=pd.read_fwf(twissum_name,skiprows=6)
         twiss_sum=twiss_sum.drop(index=0,columns='*')
         
-        data=twiss_sum.loc[:,twiss_sum.columns.isin([ 'ALPHA_C', 'ALPHA_C_P','ALPHA_C_P2','ALPHA_C_P3','DQ1','DQ2'])].astype(np.float64)
+        data=twiss_sum.loc[:,twiss_sum.columns.isin([ 'ALPHA_C', 'ALPHA_C_P','ALPHA_C_P2','ALPHA_C_P3','DQ1','DQ2','ORBIT_X','ORBIT_PX'])].astype(np.float64)
         data.insert(0,"name",k)
         data.insert(1,"k3",j)
         data.insert(2,"ALFX",twiss.ALFX[1])
@@ -42,3 +42,34 @@ for k in oct_names:
         
         alldata=alldata.append(data)
         
+        #%%
+oct_names=["LOE.12002"]#,"LOE.22002","LOE.32002","LOEN.52002"]
+strengths=[0.3,0.1]#,0.5,0.6,0.7,0.8,0.9]
+islands=["top"]#,"right","bot","left"]
+no_particles=1
+
+alldata=pd.DataFrame(columns=['name', 'k3', 'BETX', 'ALFX', 'ALPHA_C', 'ALPHA_C_P', 'ALPHA_C_P2',
+       'ALPHA_C_P3', 'DQ1', 'DQ2','ORBIT_X','ORBIT_PX'])
+
+for k in oct_names:
+    for j in strengths:
+        for i in range(len(islands)):
+            twiss_name="Data/istwiss/twiss.oct="+str(k)+"k3=" +str(j)+islands[i]+".tfs"
+            twissum_name="Data/istwiss/twissum.oct="+str(k)+"k3=" +str(j)+islands[i]+".tfs"
+            
+            twiss=pd.read_fwf(twiss_name,skiprows=88,infer_nrows=3000)
+            twiss=twiss.drop(index=0)
+            twiss=twiss.loc[:, ~twiss.columns.isin(['* NAME', 'KEYWORD'])].astype(np.float64)
+            
+            twiss_sum=pd.read_fwf(twissum_name,skiprows=6)
+            twiss_sum=twiss_sum.drop(index=0,columns='*')
+            
+            data=twiss_sum.loc[:,twiss_sum.columns.isin([ 'ALPHA_C', 'ALPHA_C_P','ALPHA_C_P2','ALPHA_C_P3','DQ1','DQ2','ORBIT_X','ORBIT_PX'])].astype(np.float64)
+            data.insert(0,"name",k)
+            data.insert(1,"k3",j)
+            data.insert(2,"island",islands[i])
+            data.insert(3,"ALFX",twiss.ALFX[1])
+            data.insert(3,"BETX",twiss.BETX[1])
+            
+            alldata=alldata.append(data)
+alldata.to_csv("islands_twiss.csv")
---
+++ trackislands_rough.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar  2 11:48:57 2023
+checked works with current setting
+@author: sawang
+"""
+
+from cpymad.madx import Madx
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+import os
+mad=Madx()
+
+
+oct_names=["LOE.12002","LOE.22002","LOE.32002","LOEN.52002"]
+strengths=[0.3,0.1,0.5,0.6,0.7,0.8,0.9]
+no_particles=8
+islands=["top","right","bot","left"]
+
+FP=[[[-0.016,0.0008],[0.025,-0.00049],[0.015,-0.00075],[-0.026,0.0005]],
+    [[-0.028,0.0009],[0.0044,0.0003],[0.029,-0.0009],[-0.008,0.00025]],
+    [[-0.017,0.0008],[0.025,-0.0005],[0.014,-0.0008],[-0.026,0.0005]],
+    [[-0.017,0.0008],[0.025,-0.0005],[0.014,-0.0008],[-0.026,0.0005]]]
+
+for k in range(len(oct_names)):
+    for j in range(len(strengths)):
+        for i in range(len(islands)):
+            x0=FP[k][i][0]
+            px0=FP[k][i][1]
+            
+           
+            with open('track_island.madx', 'r') as file:
+                data = file.read()
+                data = data.replace("K3=0.1", "K3="+str(strengths[j]))
+                data = data.replace("LOF.30802", oct_names[k])
+                data = data.replace("x=0.02", "x="+str(x0)+"+n")
+                data = data.replace("px=0.0", "px="+str(px0))
+                
+            with open('track_island.madx', 'w') as file:     
+                file.write(data)
+                
+            mad.call("track_island.madx")
+            
+            for n in range (1,no_particles+1):
+                if i <10:
+                    name="track.obs0001.p000"+str(n)
+                else:   
+                    name="track.obs0001.p00"+str(n)
+                    
+                newname="track.oct="+oct_names[k]+"k3=" +str(strengths[j])+islands[i]+"no="+str(n)
+                os.rename(name, newname)
+                
+                
+            with open('track_island.madx', 'r') as file:
+                data = file.read()
+                data = data.replace("K3="+str(strengths[j]),"K3=0.1")
+                data = data.replace(oct_names[k],"LOF.30802")
+                data = data.replace("x="+str(x0)+"+n","x=0.02")
+                data = data.replace("px="+str(px0),"px=0.0")
+            with open('track_island.madx', 'w') as file:     
+                file.write(data)
+       
+            
+
