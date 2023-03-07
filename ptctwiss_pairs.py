c5b00bfe749ce5cefce7ca6a2b343b9aa6d19a02
sabrinawaa <sabrinaziqiwang@yahoo.com>
2023-03-07 14:46:00 +0100

    s

--- ptctwiss_pairs.py
+++ ptctwiss_pairs.py
@@ -18,7 +18,7 @@ from cpymad.madx import Madx
 
 import os
 mad=Madx()
-job='pairs.madx'
+job='ptctwiss_pairs.madx'
 
 oct_names=[["LOE.12002","LOE.22002"],["LOE.22002","LOE.32002"],["LOE.22002","LOE.52002"]]
 strengths=[0.6]
@@ -43,7 +43,7 @@ FP=[
 for k in range(len(oct_names)):
     for j in range(len(strengths)):
         for i in range(len(islands)):
-            with open('job2.madx', 'r') as file:
+            with open(job, 'r') as file:
                 data = file.read()
                 data = data.replace("K3=0.1", "K3="+str(strengths[j]))
                 data = data.replace("oct1", oct_names[k][0])
@@ -51,10 +51,10 @@ for k in range(len(oct_names)):
                 data = data.replace("x=0.02", "x="+str(FP[k][i][0]))
                 data = data.replace("px=0.0", "px="+str(FP[k][i][1]))
                 
-            with open('job2.madx', 'w') as file:     
+            with open(job, 'w') as file:     
                 file.write(data)
                 
-            mad.call("job2.madx")
+            mad.call(job)
             
            
             twiss_newname="twiss.oct="+oct_names[k][0]+","+oct_names[k][1]+"k3=" +str(strengths[j])+islands[i]+".tfs"
@@ -62,14 +62,14 @@ for k in range(len(oct_names)):
             os.rename("ptc_twiss.tfs", twiss_newname)
             os.rename("ptc_twiss_summ.tfs", twissum_newname)
                 
-            with open('job2.madx', 'r') as file:
+            with open(job, 'r') as file:
                 data = file.read()
                 data = data.replace("K3="+str(strengths[j]),"K3=0.1")
                 data = data.replace( oct_names[k][0],"oct1")
                 data = data.replace(oct_names[k][1],"oct2")
                 data = data.replace("x="+str(FP[k][i][0]),"x=0.02")
                 data = data.replace("px="+str(FP[k][i][1]),"px=0.0")
-            with open('job2.madx', 'w') as file:     
+            with open(job, 'w') as file:     
                 file.write(data)
        
                     
