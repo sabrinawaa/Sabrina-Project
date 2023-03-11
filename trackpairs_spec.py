0dda2a03ef75846cf3e3e534e2d6993244839a8c
sabrinawaa <sabrinaziqiwang@yahoo.com>
2023-03-11 23:38:03 +0100

    run

---
+++ trackpairs_spec.py
@@ -0,0 +1,65 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 11 23:32:19 2023
+
+@author: sabo4ever
+"""
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 20 11:30:00 2023
+
+@author: sabo1ever
+"""
+
+from cpymad.madx import Madx
+import os
+
+
+mad=Madx()
+job='square_pairs.madx'
+
+
+      
+oct_names=[["LOE.12002","LOE.32002"],["LOE.22002","LOE.32002"],["LOE.22002","LOEN.52002"],["LOE.32002","LOEN.52002"]]
+strengths=[0.6]#
+no_particles=10
+for k in range (len(oct_names)):
+      
+        print (oct_names[k])
+        for j in range(len(strengths)):
+            with open(job, 'r') as file:
+                data = file.read()
+                data = data.replace("K3=0.1", "K3="+str(strengths[j]))
+                data = data.replace("oct1", oct_names[k][0])
+                data = data.replace("oct2",oct_names[k][1])
+                data = data.replace("oct3",oct_names[k][2])
+            with open(job, 'w') as file:     
+                file.write(data)
+                
+            mad.call(job)
+            
+            for i in range (1,no_particles+1):
+                if i <10:
+                    name="track.obs0001.p000"+str(i)
+                else:   
+                    name="track.obs0001.p00"+str(i)
+                    
+                newname="track.oct="+oct_names[k][0]+","+oct_names[k][1]+"k3=" +str(strengths[j])+"no="+str(i)
+                os.rename(name, newname)
+                
+                
+            with open(job, 'r') as file:
+                data = file.read()
+                data = data.replace("K3="+str(strengths[j]),"K3=0.1")
+                data = data.replace( oct_names[k][0],"oct1")
+                data = data.replace(oct_names[k][1],"oct2")
+                data = data.replace(oct_names[k][2],"oct3")
+            with open(job, 'w') as file:     
+                file.write(data)
+                
+ 
+
+            
