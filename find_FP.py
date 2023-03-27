b3bca0f4252e48ed51e0fc9d3e9fd75b24381bd8
sabrinawaa <sabrinaziqiwang@yahoo.com>
2023-03-27 15:46:40 +0200

    new

--- find_FP.py
+++ find_FP.py
@@ -20,7 +20,7 @@ oct_names=["LOE.12002","LOE.22002","LOE.32002","LOEN.52002"]
 strengths=[0.6]
 no_particles=8
 no_turns=2048
-#%%single
+#%%single oct
 
 twiss=pd.read_fwf('sps.tfs',skiprows=50,infer_nrows=3000)
 twiss=twiss.drop(index=0)
@@ -41,7 +41,7 @@ for k in oct_names:
             plt.ylabel("p_X")
             
 #%%pairs
-oct_names=["LOE.12002","LOE.22002"]#,"LOE.32002","LOEN.52002"]
+oct_names=["LOE.12002","LOE.22002","LOE.32002","LOEN.52002"]
 strengths=[0.6]
 no_particles=7
 for a in range (len(oct_names)):
@@ -50,7 +50,7 @@ for a in range (len(oct_names)):
             print (oct_names[k],oct_names[a])
             for j in range(len(strengths)):
                 for i in range (1,no_particles+1):
-                    name="Data/75pairs/track.oct="+oct_names[k]+","+oct_names[a]+"k3=" +str(strengths[j])+"no="+str(i)
+                    name="Data/75pairs2/track.oct="+oct_names[k]+","+oct_names[a]+"k3=" +str(strengths[j])+"no="+str(i)
                     track = pd.read_fwf(name, skiprows=6,infer_nrows=no_turns)
                     track = track.drop(index = 0,columns="*")
                     track = track.astype(np.float64)
