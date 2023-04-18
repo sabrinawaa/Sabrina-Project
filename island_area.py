04f6175edc735ad0a3449eb25400526f949a8c52
sabrinawaa <sabrinaziqiwang@yahoo.com>
2023-04-18 09:48:08 +0200

    stuff

--- island_area.py
+++ island_area.py
@@ -44,7 +44,7 @@ for i in range (1,no_particles+1):
     # else:
     #     name=folder+"track.obs0001.p"+str(i)
     # name = folder + "32track.no=" + str(i)
-    name = folder+ "track.oct=LOE.12002,LOEN.52002k3=-2.5no=" + str(i)
+    name = folder+ "track.oct=LOE.12002,LOEN.52002k3=-3,-2no=" + str(i)
     plt.figure(num='1')
     track = pd.read_fwf(name, skiprows=6,infer_nrows=no_turns)
     track = track.drop(index = 0,columns="*")
@@ -202,13 +202,13 @@ for i in idx:
 
 
 #%%
-folder="Data/1252Qx_749/"
+folder="Data/1252Qx_7485/"
 # twiss_FP = pd.read_csv("Data/twiss_csv/75Islandtwiss_csv/LOE.32002top_twiss.csv")
 # twiss_FP = twiss_FP[twiss_FP["k3"]==0.6]
 twiss_FP=pd.DataFrame(data= [[-0.002211155106,0.000432587081]],columns=["ORBIT_X","ORBIT_PX"])
 
 
-for i in [10]:
+for i in [11]:
     # if i <10:
     #     name=folder+"track.obs0001.p000"+str(i)
     # elif 9<i<100:   
@@ -219,14 +219,15 @@ for i in [10]:
     #     name=folder+"track.obs0001.p"+str(i)
     # name=folder[island]+"track.oct="+oct_names[0]+"k3=0.6no="+str(i)
     # name = folder + "32track.no=" + str(i+1)
-    name = folder+ "track.oct=LOE.12002,LOEN.52002k3=-2.5no=" + str(i)
+    name = folder+ "track.oct=LOE.12002,LOEN.52002k3=-3,-2no=" + str(i)
     track = pd.read_fwf(name, skiprows=6,infer_nrows=no_turns)
     track = track.drop(index = 0,columns="*")
     track = track.astype(np.float64)
     plt.scatter(track.X,track.PX,marker='.',s=0.1,label = 'k3=4.2, Qx=0.738')
     plt.legend()
     x4 = np.array(track.X[4::4]) - float(twiss_FP.ORBIT_X)
-    px4 = np.array(track.PX[4::4]) - float(twiss_FP.ORBIT_PX)-0.0003
+    px4 = np.array(track.PX[4::4]) - float(twiss_FP.ORBIT_PX)-0.0004
+    
     
     plt.scatter(x4,px4,marker='.',s=0.1)
     plt.scatter(0,0,marker='x',s=10) 
